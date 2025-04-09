3358a9be754655aba4890006ae099e14a6ba2bc3
DuarteCruz <duartecruz98@ik.me>
2025-04-09 22:01:52 +0100

    add roslaunch for drivers

--- shared_folder/mebt.sh
+++ shared_folder/mebt.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+# Make sure Wi-Fi is turned off
+sudo nmcli radio wifi off
+
+# Properly connect to the correct ethernet ports
+sudo nmcli connection up RS-Bpearl
+
+sleep 2
+
+# Start ROS core if not already running
+roscore &
+
+sleep 2  # Give some time for roscore to start
+
+# Launch the first ROS launch file
+roslaunch go1_ros_interface robot.launch connection_type:=ethernet feedback_frequency:=50 &
+
+# Launch the third ROS launch file
+roslaunch rslidar_sdk start.launch &
+
+sleep 10
+roslaunch xsens_driver xsens_driver.launch &
+
+sleep 10
+
 roslaunch ig_lio lio_velodyne_Bpearl.launch &
 sleep 10
 roslaunch octomap_server octomap_mapping.launch &
